d484aef28b04c00d556b7d1e6f30699e032238d7
joseevilasio <joseevilasio>
2022-12-14 23:30:51 +0000

    Example com CASE

---
+++ dia01/04_case.sql
@@ -0,0 +1,17 @@
+-- Databricks notebook source
+SELECT *,
+      -- Nova coluna na query com a informação da descNacionalidade, utilizando CASE
+      CASE 
+        WHEN descUF = 'SP' THEN 'paulista'
+        WHEN descUF = 'RJ' THEN 'fluminense'
+        WHEN descUF = 'PR' THEN 'paranaense'
+        ELSE 'outros'
+      END AS descNacionalidade,
+      
+      -- Nova coluna na query com a informação de se tem ou não são no nome, utilizando LIKE e coringa % para
+      CASE
+        WHEN descCidade LIKE '%sao%' THEN 'Tem são no nome'
+        ELSE 'Não tem são no nome'
+      END AS descCidadeSao
+      
+FROM silver_olist.cliente
