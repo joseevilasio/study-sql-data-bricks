6ea359ea1aee46a028a165d63c5168724a031a98
joseevilasio <joseevilasio>
2022-12-15 21:58:29 +0000

    example command group by

---
+++ dia02/02_group_by.sql
@@ -0,0 +1,18 @@
+-- Databricks notebook source
+SELECT 
+  descUF,
+  COUNT(*)
+
+FROM silver_olist.cliente
+
+GROUP BY descUF
+
+-- COMMAND ----------
+
+SELECT 
+  descUF,
+  COUNT(DISTINCT idClienteUnico)
+
+FROM silver_olist.cliente
+
+GROUP BY descUF
