8c185001a1a05f6cb276ec56c7119a8621421a6a
joseevilasio <joseevilasio>
2022-12-15 21:46:56 +0000

    update exercise

--- dia01/bora_praticar.sql
+++ dia01/bora_praticar.sql
@@ -1,2 +1,34 @@
 -- Databricks notebook source
+-- 01 Selecione todos os clientes paulistanos
+SELECT *
+FROM silver_olist.cliente
+WHERE descCidade = 'sao paulo'
 
+-- COMMAND ----------
+
+-- 02 Selecione todos os clientes paulistas
+
+SELECT * 
+FROM silver_olist.cliente
+WHERE descUF = 'SP'
+
+-- COMMAND ----------
+
+-- 03 Selecione todos vendedores cariocas e paulistas
+SELECT *
+
+FROM silver_olist.vendedor
+
+WHERE descCidade = 'rio de janeiro'
+OR descUF = 'SP'
+
+-- COMMAND ----------
+
+-- 04 Selecione produtos de perfumaria e bebes com altura maior que 5cm
+
+SELECT *
+
+FROM silver_olist.produto
+
+WHERE descCategoria IN ('perfumaria', 'bebes')
+AND vlAlturaCm > 5
