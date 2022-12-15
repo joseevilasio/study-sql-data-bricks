189ffc321882089162d8bc4b16ff417f4231f522
joseevilasio <joseevilasio>
2022-12-15 21:42:40 +0000

    update example command case

--- dia01/04_case.sql
+++ dia01/04_case.sql
@@ -7,6 +7,19 @@ SELECT *,
         WHEN descUF = 'RJ' THEN 'fluminense'
         WHEN descUF = 'PR' THEN 'paranaense'
         ELSE 'outros'
+      END AS descNacionalidade
+      
+FROM silver_olist.cliente
+
+-- COMMAND ----------
+
+SELECT *,
+      -- Nova coluna na query com a informação da descNacionalidade, utilizando CASE
+      CASE 
+        WHEN descUF = 'SP' THEN 'paulista'
+        WHEN descUF = 'RJ' THEN 'fluminense'
+        WHEN descUF = 'PR' THEN 'paranaense'
+        ELSE 'outros'
       END AS descNacionalidade,
       
       -- Nova coluna na query com a informação de se tem ou não são no nome, utilizando LIKE e coringa % para
@@ -16,3 +29,12 @@ SELECT *,
       END AS descCidadeSao
       
 FROM silver_olist.cliente
+
+-- COMMAND ----------
+
+SELECT *,
+       CASE
+         WHEN descUF IN ('SP', 'MG', 'RJ', 'ES') THEN 'sudeste'
+       END AS descRegiao
+
+FROM silver_olist.vendedor
